2e914a81f15718404bef497c348a90285b61e354
sakura miyamoto <sakura.m.208@gmail.com>
2022-02-19 08:59:03 +0900

    init

---
+++ pages/about.jsx
@@ -0,0 +1,16 @@
+import Layout from '../components/Layout'
+
+const About = () => {
+  return (
+    <Layout>
+      <h2 className="text-2xl">Aboutぺーじです</h2>
+      <p>あばうと</p>
+      <p>あばうと</p>
+      <p>あばうと</p>
+      <p>あばうと</p>
+      <p>あばうと</p>
+    </Layout>
+  )
+}
+
+export default About;
