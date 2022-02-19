fd7b3a616b270c7dec4fbb333be40c9e2d9b2243
sakura miyamoto <sakura.m.208@gmail.com>
2022-02-19 10:07:52 +0900

    test

--- pages/index.js
+++ pages/index.js
@@ -7,21 +7,21 @@ import { db } from "../src/firebase";
 import { collection, getDocs } from "firebase/firestore";
 
 export default function Home() {
-  // const [posts, setPosts] = useState([]);
-  // useEffect(() => {
-  //   const postsCollectionRef = collection(db, "posts");
-  //   getDocs(postsCollectionRef).then((querySnapshot) => {
-  //     setPosts(querySnapshot.docs.map((doc) => doc.data()));
-  //   });
-  // }, []);
-  // console.log(posts);
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const postsCollectionRef = collection(db, "posts");
+    getDocs(postsCollectionRef).then((querySnapshot) => {
+      setPosts(querySnapshot.docs.map((doc) => doc.data()));
+    });
+  }, []);
+  console.log(posts);
 
   return (
     <div className={styles.container}>
       <Layout title="ホームのページです">
         <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
           <div className="grid gap-5 lg:grid-cols-3 sm:max-w-sm sm:mx-auto lg:max-w-full">
-            {/* {posts.map((post, index) => {
+            {posts.map((post, index) => {
               return (
                 <Card
                   key={index}
@@ -32,7 +32,7 @@ export default function Home() {
                   url={post.url}
                 />
               );
-            })} */}
+            })}
             <Card
               date="2022/2/12"
               src={imgSrc}
