d3e1d4f50caad241212405b4ab78ddead2617024
sakura miyamoto <sakura.m.208@gmail.com>
2022-02-19 10:36:36 +0900

    aaaa

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
       <Layout title="ホームのページです">
         <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
           <div className="grid gap-5 lg:grid-cols-3 sm:max-w-sm sm:mx-auto lg:max-w-full">
-            {/* {posts.map((post, index) => {
+            {posts.map((post, index) => {
               return (
                 <Card
                   key={index}
@@ -31,7 +31,7 @@ export default function Home() {
                   url={post.url}
                 />
               );
-            })} */}
+            })}
             <Card
               date="2022/2/12"
               src={imgSrc}
