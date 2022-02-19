70b1bb21379317864cb6bca8a1d521556a4ccfcf
sakura miyamoto <sakura.m.208@gmail.com>
2022-02-19 10:23:45 +0900

    a

--- pages/upload.jsx
+++ pages/upload.jsx
@@ -81,7 +81,7 @@ const Upload = () => {
           <div className="col-span-2 text-right">
             <button
               className="py-2 px-4  bg-blue-400 hover:bg-blue-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
-              onClick={handleSubmit}
+              // onClick={handleSubmit}
             >
               投稿
             </button>
